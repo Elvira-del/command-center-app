ee06c7687e99c8fe95f1a3676e7811c2cea0f4f4
Elvira-del <ninelostr@gmail.com>
2023-03-01 17:01:59 +0300

    feat: add styled-components and base theme for app

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  borderRadius: "10px",
+
+  colors: {
+    primary: "#5d8c56",
+    secondary: "#2a403a",
+
+    font: "#070d0c",
+  },
+
+  width: {
+    container: 1440,
+  },
+};
