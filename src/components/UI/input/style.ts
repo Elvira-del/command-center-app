e8d8a06f6cacba6701404f9b05368a5fd4f4ed97
Elvira-del <ninelostr@gmail.com>
2023-03-02 03:25:00 +0300

    fix: change elements height

--- src/components/UI/input/style.ts
+++ src/components/UI/input/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Input = styled.input`
   padding: 10px;
+  height: 100%;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: rgba(9, 9, 9, 0.1);
 `;
--- src/components/UI/select/style.ts
+++ src/components/UI/select/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Select = styled.select`
   padding: 10px;
+  height: 100%;
   border-radius: ${({ theme }) => theme.borderRadius};
   background-color: rgba(9, 9, 9, 0.1);
 `;
