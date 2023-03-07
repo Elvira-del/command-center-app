4723ff419e292669c7eb7e302276940a04619a9c
Elvira-del <ninelostr@gmail.com>
2023-03-08 00:26:45 +0300

    refactor: add data for app

---
+++ src/data.ts
@@ -0,0 +1,12 @@
+export const LocationDict = [
+  { id: "earth", value: "Earth" },
+  { id: "mars", value: "Mars" },
+  { id: "venus", value: "Venus" },
+  { id: "pluto", value: "Pluto" },
+];
+
+export const IssueStatusDict = [
+  { id: "actual", value: "Actual" },
+  { id: "handled", value: "Handled" },
+  { id: "archived", value: "Archived" },
+];
