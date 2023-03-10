a8872669a610107b05ea60eb15c70c8b86f9b2a4
Elvira-del <ninelostr@gmail.com>
2023-03-10 23:17:58 +0300

    Updates

--- src/components/atoms/input/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Input = styled.input`
-  padding: 10px;
-  height: 100%;
-  border-radius: ${({ theme }) => theme.borderRadius};
-  background-color: ${({ theme }) => theme.colors.bgElem};
-`;
--- src/components/atoms/select/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Select = styled.select`
-  padding: 10px;
-  height: 100%;
-  border-radius: ${({ theme }) => theme.borderRadius};
-  background-color: ${({ theme }) => theme.colors.bgElem};
-`;
